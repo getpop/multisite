4542f3439ea47cc2ab86121ae1494bed237e9bb7
leoloso <leo@getpop.org>
2021-09-13 07:59:50 +0000

    Merge pull request #1033 from leoloso/Move-FieldResolvers-under-ObjectType-and-InterfaceType
    Move FieldResolvers under folders `ObjectType` and `InterfaceType`

--- src/FieldResolvers/ObjectType/SiteFieldResolver.php
+++ src/FieldResolvers/ObjectType/SiteFieldResolver.php
@@ -2,47 +2,57 @@
 
 declare(strict_types=1);
 
-namespace PoP\Multisite\FieldResolvers;
+namespace PoP\Multisite\FieldResolvers\ObjectType;
 
-use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
+use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractDBDataFieldResolver;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
-use PoP\Engine\TypeResolvers\ObjectType\RootTypeResolver;
-use PoP\Multisite\ObjectFacades\SiteObjectFacade;
 use PoP\Multisite\TypeResolvers\ObjectType\SiteTypeResolver;
 
-class RootFieldResolver extends AbstractDBDataFieldResolver
+class SiteFieldResolver extends AbstractDBDataFieldResolver
 {
     public function getObjectTypeResolverClassesToAttachTo(): array
     {
         return [
-            RootTypeResolver::class,
+            SiteTypeResolver::class,
         ];
     }
 
     public function getFieldNamesToResolve(): array
     {
         return [
-            'sites',
-            'site',
+            'domain',
+            'host',
         ];
+    }
+
+    public function getSchemaFieldType(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): string
+    {
+        $types = [
+            'domain' => SchemaDefinition::TYPE_STRING,
+            'host' => SchemaDefinition::TYPE_STRING,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($objectTypeResolver, $fieldName);
     }
 
     public function getSchemaFieldTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?int
     {
-        return match ($fieldName) {
-            'site' => SchemaTypeModifiers::NON_NULLABLE,
-            'sites' => SchemaTypeModifiers::NON_NULLABLE | SchemaTypeModifiers::IS_ARRAY,
-            default => parent::getSchemaFieldTypeModifiers($objectTypeResolver, $fieldName),
-        };
+        $nonNullableFieldNames = [
+            'domain',
+            'host',
+        ];
+        if (in_array($fieldName, $nonNullableFieldNames)) {
+            return SchemaTypeModifiers::NON_NULLABLE;
+        }
+        return parent::getSchemaFieldTypeModifiers($objectTypeResolver, $fieldName);
     }
 
     public function getSchemaFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
     {
         $descriptions = [
-            'sites' => $this->translationAPI->__('All websites', 'multisite'),
-            'site' => $this->translationAPI->__('This website', 'multisite'),
+            'domain' => $this->translationAPI->__('The site\'s domain', ''),
+            'host' => $this->translationAPI->__('The site\'s host', ''),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($objectTypeResolver, $fieldName);
     }
@@ -62,27 +72,14 @@ class RootFieldResolver extends AbstractDBDataFieldResolver
         ?array $expressions = null,
         array $options = []
     ): mixed {
-        $root = $resultItem;
+        $site = $resultItem;
         switch ($fieldName) {
-            case 'sites':
-                $site = SiteObjectFacade::getInstance();
-                return [$site->getID()];
-            case 'site':
-                $site = SiteObjectFacade::getInstance();
-                return $site->getID();
+            case 'domain':
+                return $site->getDomain();
+            case 'host':
+                return $site->getHost();
         }
 
         return parent::resolveValue($objectTypeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
-    }
-
-    public function getFieldTypeResolverClass(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
-    {
-        switch ($fieldName) {
-            case 'sites':
-            case 'site':
-                return SiteTypeResolver::class;
-        }
-
-        return parent::getFieldTypeResolverClass($objectTypeResolver, $fieldName);
     }
 }
